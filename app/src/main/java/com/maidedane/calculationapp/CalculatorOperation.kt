ab2db36ea4f1e929005e6e9265d460e0a6302054
maidane <maidedane89@gmail.com>
2022-12-19 20:42:07 +0300

    Calculator

---
+++ app/src/main/java/com/maidedane/calculationapp/CalculatorOperation.kt
@@ -0,0 +1,8 @@
+package com.maidedane.calculationapp
+
+sealed class CalculatorOperation(val symbol: String) {
+    object Add : CalculatorOperation("+")
+    object Subtract : CalculatorOperation("-")
+    object Multiply : CalculatorOperation("x")
+    object Divide : CalculatorOperation("/")
+}
---
+++ app/src/main/java/com/maidedane/calculationapp/CalculatorState.kt
@@ -0,0 +1,7 @@
+package com.maidedane.calculationapp
+
+data class CalculatorState(
+    val number1: String = "",
+    val number2: String = "",
+    val operation: CalculatorOperation? = null
+)
